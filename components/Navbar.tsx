7d48e51170e50059d5e6bf6c5f2b92ae0232a0a6
Siddharth <siddharth12dhiman@gmail.com>
2024-06-07 20:52:56 +0530

    started work on navbar and found logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+
+      <Link href="/" className='flex items-center gap-1'>
+      <Image 
+        src="/icons/logo.png"
+        width={50}
+        height={50}
+        alt="MeetEase"
+      />
+      </Link>
+
+    </nav>
   )
 }
 
