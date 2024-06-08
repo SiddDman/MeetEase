0d7d787325817641a5784582ee9066baa5070f3d
Siddharth <siddharth12dhiman@gmail.com>
2024-06-08 23:21:57 +0530

    minor change

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,7 @@ export const metadata: Metadata = {
   }
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
       <ClerkProvider
